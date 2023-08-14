994842597db3a27bd3273bdf19319ebfaebcdb5d
abiolafasanya <hezekiahfasanya@gmail.com>
2023-08-14 13:19:13 +0100

    up: added share icon

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,22 @@
 import Socials from "./main/Socials";
 import styles from "./Main.module.scss";
 import Footer from "./Footer";
-import ProfilePic from '../../public/profile__img.svg'
+import ProfilePic from "../../public/profile__img.svg";
+import ShareArrow from "../../public/shareArrow.svg";
 
 const Main = () => {
   return (
     <div className={styles.main}>
-      <div className={styles.main_profile}>
+      <article className={styles.main_profile}>
         <img src={ProfilePic} alt="profile image" />
         <h5>John Doe</h5>
+      <div className={styles.main_shareIcon}>
+        <img src={ShareArrow} alt="share arrow" />
+        <span>...</span>
       </div>
-        <Socials />
-        <Footer />
+      </article>
+      <Socials />
+      <Footer />
     </div>
   );
 };
